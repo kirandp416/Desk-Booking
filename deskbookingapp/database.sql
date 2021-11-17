0f70443cda3826620b5121e88a720bdf9395180a
Hassan Ishfaq <ishfaqh@cardiff.ac.uk>
2021-11-17 17:15:13 +0000

    Add SQL to create database and a room table

---
+++ deskbookingapp/database.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS deskbooking;
+
+CREATE DATABASE deskbooking;
+
+USE deskbooking;
+
+CREATE TABLE room
+(
+    room_id   INT PRIMARY KEY AUTO_INCREMENT,
+    room_name VARCHAR(255) NOT NULL
+);
