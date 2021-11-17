45c8e51b6c13841df5135e0972159011f478c29b
HUGH O'CONNOR <OCONNORHA@cardiff.ac.uk>
2021-11-17 13:51:36 +0000

    Created project. Created basic homepage controller and page. Gitignored application.properties.

---
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/DeskbookingappApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.nsa.team2.deskbookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskbookingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeskbookingappApplication.class, args);
+    }
+
+}
