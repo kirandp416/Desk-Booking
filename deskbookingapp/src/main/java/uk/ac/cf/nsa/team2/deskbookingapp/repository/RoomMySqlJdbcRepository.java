a2af6cb2c0d85dcc7be3d89e029a31593157d207
Hassan Ishfaq <ishfaqh@cardiff.ac.uk>
2021-11-17 18:50:18 +0000

    Implement repository for room table

---
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/repository/RoomMySqlJdbcRepository.java
@@ -0,0 +1,38 @@
+package uk.ac.cf.nsa.team2.deskbookingapp.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import uk.ac.cf.nsa.team2.deskbookingapp.dto.RoomDTO;
+import uk.ac.cf.nsa.team2.deskbookingapp.model.Room;
+
+import java.util.Optional;
+
+/**
+ * An implementation of the {@link RoomRepository} which uses
+ * a MySQL database as the underlying store via JDBC.
+ */
+public class RoomMySqlJdbcRepository implements RoomRepository {
+
+    private final JdbcTemplate jdbc;
+
+    @Autowired
+    public RoomMySqlJdbcRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    @Override
+    public boolean add(RoomDTO room) {
+        final String sql = "INSERT INTO room(room_name) VALUES(?)";
+        int rowsAffected = 0;
+
+        try {
+            rowsAffected = jdbc.update(sql, room.getName());
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+
+        return rowsAffected == 1;
+    }
+
+}
---
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/repository/RoomRepository.java
@@ -0,0 +1,21 @@
+package uk.ac.cf.nsa.team2.deskbookingapp.repository;
+
+import uk.ac.cf.nsa.team2.deskbookingapp.dto.RoomDTO;
+import uk.ac.cf.nsa.team2.deskbookingapp.model.Room;
+
+import java.util.Optional;
+
+/**
+ * A repository which manages the data for rooms in the underlying store.
+ */
+public interface RoomRepository {
+
+    /**
+     * Adds a new room to the underlying store.
+     *
+     * @param room The room to add.
+     * @return true if the operation succeeded, false otherwise.
+     */
+    boolean add(RoomDTO room);
+
+}
