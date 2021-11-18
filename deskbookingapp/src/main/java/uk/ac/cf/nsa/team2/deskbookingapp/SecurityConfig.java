6124f934b980ef15d76f54b2812d2ff4a6f8725c
Poorna Kiran Dasari <dasarip@cardiff.ac.uk>
2021-11-18 12:28:37 +0000

    Issue 19:Added Login and Logout functionality

---
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/SecurityConfig.java
@@ -0,0 +1,40 @@
+package uk.ac.cf.nsa.team2.deskbookingapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//Create Web Security for Login and Logout flows. Added redirection after logout
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/Home").permitAll()
+                .mvcMatchers("/Admin").hasRole("ADMIN")
+                .mvcMatchers("/booking").hasRole("USER")
+                .anyRequest().denyAll()
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll();
+
+    }
+
+//Adding user roles and their details for in memory auth
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("{noop}admin").roles("ADMIN")
+                .and()
+                .withUser("user").password("{noop}user").roles("USER");
+    }
+
+}
--- deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/controllers/MainController.java
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/controllers/MainController.java
@@ -1,7 +1,9 @@
 package uk.ac.cf.nsa.team2.deskbookingapp.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class MainController {
@@ -10,4 +12,15 @@ public class MainController {
     public String home(){
         return "Home";
     }
+
+    @RequestMapping(path = "Admin")
+    public String adminStuff(String name) {
+        return "Admin";
+    }
+
+    @RequestMapping(path = "booking")
+    public String booking(String name) {
+        return "booking";
+    }
+
 }
