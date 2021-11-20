2effde526c49083fb47c1a65a78ea5e3ad2c4203
HUGH O'CONNOR <OCONNORHA@cardiff.ac.uk>
2021-11-20 16:43:22 +0000

    Have removed WebSecurityConfig.java which I had in my branch which was clashing with SecurityConfig.java. Created some links on homepage so that site can be tested easily. Logout and Home buttons are now on all main pages.

--- deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/security/SecurityConfig.java
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package uk.ac.cf.nsa.team2.deskbookingapp;
+package uk.ac.cf.nsa.team2.deskbookingapp.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
