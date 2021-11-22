9370ed5d492eb3a251f09869ef6772f01d83b51b
yitingzheng <zhengy42@cardiff.ac.uk>
2021-11-22 12:09:39 +0000

    file upload and download

---
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/response/UploadFileResponse.java
@@ -0,0 +1,32 @@
+package uk.ac.cf.nsa.team2.deskbookingapp.response;
+
+import java.io.Serializable;
+
+/**
+ * @description:
+ * @author: yiting zheng
+ * @time: 2021/11/20
+ */
+public class UploadFileResponse implements Serializable {
+    private final String fileName;
+    private final String fileDownloadUri;
+    private final String fileType;
+    private final long size;
+
+    public UploadFileResponse(String fileName, String fileDownloadUri, String fileType, long size) {
+        this.fileName = fileName;
+        this.fileDownloadUri = fileDownloadUri;
+        this.fileType = fileType;
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "fileName='" + fileName + '\'' +
+                ", fileDownloadUri='" + fileDownloadUri + '\'' +
+                ", fileType='" + fileType + '\'' +
+                ", size=" + size +
+                '}';
+    }
+}
