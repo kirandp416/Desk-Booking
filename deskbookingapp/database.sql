f7b2a6df2e04685a93ea4e9712f60335524645e3
Hassan Ishfaq <ishfaqh@cardiff.ac.uk>
2021-11-23 10:38:09 +0000

    Update SQL script: Add desk type table, add desk type and note fields to desk

--- deskbookingapp/database.sql
+++ deskbookingapp/database.sql
@@ -10,24 +10,41 @@ CREATE TABLE room
     room_name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE desk_type
+(
+    desk_type_id   INT AUTO_INCREMENT,
+    desk_type_name VARCHAR(255) NOT NULL,
+    CONSTRAINT desk_type_pk_index PRIMARY KEY (desk_type_id)
+);
+
 CREATE TABLE desk
 (
-    desk_id   INT AUTO_INCREMENT,
-    room_id   INT,
-    desk_name VARCHAR(255) NOT NULL,
+    desk_id      INT AUTO_INCREMENT,
+    room_id      INT,
+    desk_type_id INT,
+    desk_name    VARCHAR(255) NOT NULL,
+    notes        VARCHAR(255) NOT NULL,
     CONSTRAINT desk_pk_index PRIMARY KEY (desk_id),
+    CONSTRAINT desk_desk_type_id FOREIGN KEY (desk_type_id) REFERENCES desk_type (desk_type_id),
     CONSTRAINT desk_room_fk_index FOREIGN KEY (room_id) REFERENCES room (room_id)
 );
 
 CREATE TABLE booking
 (
     booking_id   INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
+    username     VARCHAR(255) NOT NULL,
     booking_date DATE
 );
 
-INSERT INTO booking (username, booking_date) VALUES ("user1", '2021-11-12');
-INSERT INTO booking (username, booking_date) VALUES ("user1", '2022-03-26');
-INSERT INTO booking (username, booking_date) VALUES ("user2", '2023-10-01');
-INSERT INTO booking (username, booking_date) VALUES ("user2", '2026-06-20');
+INSERT INTO desk_type (desk_type_id desk_type_name)
+VALUES (1, 'Standard'),
+       (2, 'Standing');
 
+INSERT INTO booking (username, booking_date)
+VALUES ("user1", '2021-11-12');
+INSERT INTO booking (username, booking_date)
+VALUES ("user1", '2022-03-26');
+INSERT INTO booking (username, booking_date)
+VALUES ("user2", '2023-10-01');
+INSERT INTO booking (username, booking_date)
+VALUES ("user2", '2026-06-20');
