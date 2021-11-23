9f9c7c1550ef4e4cdb8604c298af18e03d346038
Poorna Kiran Dasari <dasarip@cardiff.ac.uk>
2021-11-23 17:36:11 +0000

    Added a new page to view all rooms.

--- deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/controllers/RoomAdminController.java
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/controllers/RoomAdminController.java
@@ -5,10 +5,16 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import uk.ac.cf.nsa.team2.deskbookingapp.dto.RoomDTO;
 import uk.ac.cf.nsa.team2.deskbookingapp.form.RoomForm;
 import uk.ac.cf.nsa.team2.deskbookingapp.repository.RoomRepository;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class RoomAdminController {
@@ -48,6 +54,28 @@ public class RoomAdminController {
         // Return a model and view, passing in the result of the operation to the view.
         return new ModelAndView("admin/add_room")
                 .addObject("result", result);
+    }
+    /**
+     * Create a Model and View object that contains all of the rooms
+     * that have been made by a particular user in the Model and the
+     * rooms page as the View
+     *
+     * @return a Model and View object
+     */
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(path = "/admin/rooms/all", method = RequestMethod.GET)
+    public ModelAndView getAllRooms() {
+            // Get rooms from repository.
+            Optional<List<RoomDTO>> rooms = roomRepository.findAll();
+            // If the optional is empty, redirect user to server error page.
+            if (rooms.isEmpty()) {
+                return new ModelAndView("redirect:/internal_server_error");
+            }
+
+            // Return a model and view for the manage desks page,
+            // passing the rooms into the view.
+            return new ModelAndView("admin/manage_rooms")
+                    .addObject("rooms", rooms.get());
     }
 
 }
