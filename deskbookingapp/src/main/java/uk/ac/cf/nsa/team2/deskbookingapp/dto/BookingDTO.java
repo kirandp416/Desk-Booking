532ae8e8519147d08a108545aa886b94ca9220e7
HUGH O'CONNOR <OCONNORHA@cardiff.ac.uk>
2021-11-27 15:37:05 +0000

    Created an extra field in BookingDTO so that each booking has a user-friendly formatted date string in form dd-mm-yy instead of using the date as it was on the database which was yyyy-mm-dd.

--- deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/dto/BookingDTO.java
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/dto/BookingDTO.java
@@ -1,5 +1,9 @@
 package uk.ac.cf.nsa.team2.deskbookingapp.dto;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * A class that will be used to instantiate Objects
  * that each represent one booking made in our system.
@@ -8,6 +12,7 @@ public class BookingDTO {
 
     private int id;
     private String date;
+    private String dateOrderedForDisplay;
     private String roomName;
     private String deskName;
     private String deskType;
@@ -20,6 +25,7 @@ public class BookingDTO {
         this.deskName = deskName;
         this.deskType = deskType;
         this.deskNotes = deskNotes;
+        this.dateOrderedForDisplay = dateReOrderer(date);
     }
 
     public int getId() {
@@ -44,5 +50,33 @@ public class BookingDTO {
 
     public String getDeskNotes() {
         return deskNotes;
+    }
+
+    public String getDateOrderedForDisplay() {
+        return dateOrderedForDisplay;
+    }
+
+    // Create a method that takes the date of the booking on the database (which
+    // is in the form yyyy-mm-dd) and reformats it to a more user-friendly format.
+    // At the time of writing this comment, the method converts the date to the
+    // form dd-mm-yy. However, developers can change this to other formats by giving
+    // the constructor for the object named dateFormatRequired, other arguments. For
+    // example, you could switch it to American dates by passing it "MM-dd-YY".
+
+    public String dateReOrderer(String dateString){
+
+        SimpleDateFormat dateFormatDB = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+
+        try {
+            date = dateFormatDB.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        SimpleDateFormat dateFormatRequired = new SimpleDateFormat("dd-MM-yy");
+
+        return dateFormatRequired.format(date);
+
     }
 }
