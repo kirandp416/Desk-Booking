c8b2cca49de23cd61d14e76c92bd44003dcd48dd
yitingzheng <zhengy42@cardiff.ac.uk>
2021-12-01 14:31:09 +0000

    rest api

---
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/dto/CommentDTO.java
@@ -0,0 +1,9 @@
+package uk.ac.cf.nsa.team2.deskbookingapp.dto;
+
+/**
+ * @description:
+ * @author: yiting zheng
+ * @time: 2021/12/1
+ */
+public class CommentDTO {
+}
---
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/rest/CommentController.java
@@ -0,0 +1,15 @@
+package uk.ac.cf.nsa.team2.deskbookingapp.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: yiting zheng
+ * @time: 2021/11/29
+ */
+@RestController
+@RequestMapping
+public class CommentController {
+
+}
