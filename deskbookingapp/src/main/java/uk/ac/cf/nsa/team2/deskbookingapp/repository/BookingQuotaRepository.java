f8cdf3f1b9661faa9d87958cb5b6b13a45484903
Hassan Ishfaq <ishfaqh@cardiff.ac.uk>
2021-12-03 17:13:12 +0000

    Implement booking quota repository methods to get booking quota for month and used quota for an employee

---
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/repository/BookingQuotaRepository.java
@@ -0,0 +1,23 @@
+package uk.ac.cf.nsa.team2.deskbookingapp.repository;
+
+import java.util.Optional;
+
+public interface BookingQuotaRepository {
+
+    /**
+     * Finds the booking quota for the current month for the specified room.
+     *
+     * @param roomId the ID of the room.
+     * @return an optional containing the quota for the current month
+     */
+    Optional<Integer> findQuotaByRoom(int roomId);
+
+    /**
+     * Finds the booking quota used by an employee for the current month.
+     *
+     * @param username the employee's username.
+     * @return an optional containing the quota used for the month by the employee if the operation was successful.
+     */
+    Optional<Integer> findUsedQuotaByEmployee(String username);
+
+}
