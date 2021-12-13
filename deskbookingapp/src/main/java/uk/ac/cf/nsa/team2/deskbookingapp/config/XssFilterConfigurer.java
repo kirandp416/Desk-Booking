f1f170522757f49fc643a7f22ddef8656e990021
yitingzheng <zhengy42@cardiff.ac.uk>
2021-12-13 11:03:31 +0000

    XssFilterConfigurer.java comments

--- deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/config/XssFilterConfigurer.java
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/config/XssFilterConfigurer.java
@@ -19,10 +19,13 @@ public class XssFilterConfigurer {
     public FilterRegistrationBean xssFilterRegistrationBean() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
         filterRegistrationBean.setFilter(new XssFilter());
+        // Set the execution order. The smaller the value, the earlier the execution.
         filterRegistrationBean.setOrder(Integer.MAX_VALUE - 1);
+        // Enable filter
         filterRegistrationBean.setEnabled(true);
         filterRegistrationBean.addUrlPatterns("/*");
         Map<String, String> initParameters = new HashMap();
+        // initParameters
         initParameters.put("excludes", "/favicon.ico,/img/*,/js/*,/css/*");
         initParameters.put("isIncludeRichText", "true");
         filterRegistrationBean.setInitParameters(initParameters);
