5ef5a287258083c95282012c5587d490dd629a95
yitingzheng <zhengy42@cardiff.ac.uk>
2021-12-15 17:37:31 +0000

    solve merge conflict about yiting and zhaoyi

--- deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/controllers/MainController.java
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/controllers/MainController.java
@@ -7,13 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import uk.ac.cf.nsa.team2.deskbookingapp.utils.MysqlCommonHandler;
-import uk.ac.cf.nsa.team2.deskbookingapp.utils.StringCommonHandler;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.HashMap;
 
 @Controller
 public class MainController {
