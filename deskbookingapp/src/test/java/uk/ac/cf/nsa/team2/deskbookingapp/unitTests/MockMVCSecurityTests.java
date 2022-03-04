48191f4f9dfc940961e8e6d34fd8c023788e8407
Poorna Kiran Dasari <dasarip@cardiff.ac.uk>
2022-03-04 22:04:06 +0000

    Simple Test

--- deskbookingapp/src/test/java/uk/ac/cf/nsa/team2/deskbookingapp/unitTests/MockMVCSecurityTests.java
+++ deskbookingapp/src/test/java/uk/ac/cf/nsa/team2/deskbookingapp/unitTests/MockMVCSecurityTests.java
@@ -5,17 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.ac.cf.nsa.team2.deskbookingapp.controllers.RoomAdminController;
 import uk.ac.cf.nsa.team2.deskbookingapp.repository.RoomRepository;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RoomAdminController.class)
@@ -28,9 +29,9 @@ public class MockMVCSecurityTests {
 
     @Test
     public void addRoom() throws Exception {
-        this.mockMvc.perform(get("/admin/room/add")
-                        .with(user("admin").password("{noop}admin").roles("ADMIN")))
-                .andDo(print()) .andExpect(status().isOk())
-               ;
+
+        this.mockMvc.perform(get("/admin/room/add"))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
     }
 }
--- deskbookingapp/src/test/java/uk/ac/cf/nsa/team2/deskbookingapp/unitTests/MockMVCTests.java
+++ deskbookingapp/src/test/java/uk/ac/cf/nsa/team2/deskbookingapp/unitTests/MockMVCTests.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.ac.cf.nsa.team2.deskbookingapp.controllers.RoomAdminController;
+import uk.ac.cf.nsa.team2.deskbookingapp.repository.RoomRepository;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -20,7 +22,8 @@ class MockMVCTests {
 
     @Autowired
     private MockMvc mockMvc;
-
+    @MockBean
+    private RoomRepository roomRepository;
 
     @Test
     public void loginScreen() throws Exception {
