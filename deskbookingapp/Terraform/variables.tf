cee126ec22a39038bb8edffb6ffa12a850276c02
Poorna Kiran Dasari <dasarip@cardiff.ac.uk>
2022-04-12 15:54:31 +0100

    Updated server

--- deskbookingapp/Terraform/variables.tf
+++ deskbookingapp/Terraform/variables.tf
@@ -10,4 +10,4 @@ variable "keypair" { default = "21091401_keypair" } # you need to change this
 variable "pool" { default = "cscloud_private_floating" }
 variable "server_script" { default = "./DebianServer.sh" }
 variable "security_description" { default = "Terraform security group" }
-variable "security_name" { default = "tf_security" }
+variable "security_name" { default = "tf_security_deskbooking" }
