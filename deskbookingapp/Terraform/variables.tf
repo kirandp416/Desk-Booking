f508e14f02b9664ad4b245a0fc9d381cc3fb0d66
Poorna Kiran Dasari <dasarip@cardiff.ac.uk>
2022-04-12 15:41:09 +0100

    added Terraform files

--- deskbookingapp/Terraform/variables.tf
+++ deskbookingapp/Terraform/variables.tf
@@ -2,12 +2,12 @@ variable "flavor" { default = "m1.large" }
 variable "image" { default = "Debian Buster 10.11.1 20211029" }
 #variable "instance" { default = "tf_instance" }
 
-variable "name" { default = "JenkinsServer" }
+variable "name" { default = "DebianServer" }
 
 variable "network" { default = "default" }   # you need to change this
 
 variable "keypair" { default = "21091401_keypair" } # you need to change this
 variable "pool" { default = "cscloud_private_floating" }
-variable "server_script" { default = "./serverJenkins.sh" }
+variable "server_script" { default = "./DebianServer.sh" }
 variable "security_description" { default = "Terraform security group" }
 variable "security_name" { default = "tf_security" }
