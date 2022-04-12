c1fe6720a1ba1965624353e4e32c18c2f152914b
Poorna Kiran Dasari <dasarip@cardiff.ac.uk>
2022-04-12 17:11:04 +0100

    Updated server

--- deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/Application.java
+++ deskbookingapp/src/main/java/uk/ac/cf/nsa/team2/deskbookingapp/Application.java
@@ -1,7 +1,10 @@
 package uk.ac.cf.nsa.team2.deskbookingapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class Application {
